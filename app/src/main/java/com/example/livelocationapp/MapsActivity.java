893662baf4f0c698751a22c973117b27582be97f
Bhavy <bhavy2908@gmail.com>
2021-03-22 16:54:46 +0530

    added realtime location support

--- app/src/main/java/com/example/livelocationapp/MapsActivity.java
+++ app/src/main/java/com/example/livelocationapp/MapsActivity.java
@@ -1,30 +1,111 @@
 package com.example.livelocationapp;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
 
+    private DatabaseReference reference;
+    private LocationManager manager;
+    private final int MIN_TIME = 1000; // 1 second
+    private final int MIN_DISTANCE = 1; // 1 meter
+    Marker myMarker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        FirebaseDatabase.getInstance().getReference().setValue("Working Perfectly");
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        reference = FirebaseDatabase.getInstance().getReference().child("User-101");
+        //FirebaseDatabase.getInstance().getReference().setValue("Working Perfectly");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        getLocationUpdates();
+        readChanges();
+    }
+
+    private void readChanges() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    try {
+
+                        MyLocation location = snapshot.getValue(MyLocation.class);
+                        if (location != null) {
+                            myMarker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                        }
+                    }
+                    catch (Exception e){
+                        Toast.makeText(MapsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getLocationUpdates() {
+        if (manager != null){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else {
+                    Toast.makeText(this, "Please Enable Location", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLocationUpdates();
+            }
+            else {
+                Toast.makeText(this, "Permission REquired", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     /**
@@ -42,7 +123,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng NewDelhi = new LatLng(28.8040562, 76.1318998);
-        mMap.addMarker(new MarkerOptions().position(NewDelhi).title("You are Here"));
+        myMarker = mMap.addMarker(new MarkerOptions().position(NewDelhi).title("You are Here"));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(NewDelhi));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if (location != null){
+            saveLocation(location);
+        }
+        else {
+            Toast.makeText(this, "No Location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveLocation(Location location) {
+        reference.setValue(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 }
