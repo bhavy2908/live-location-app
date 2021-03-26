8c16ea175be23b2db8884a256b84cf2d36b18d85
Bhavy <bhavy2908@gmail.com>
2021-03-26 23:29:42 +0530

    Update MapsActivity.java

--- app/src/main/java/com/example/livelocationapp/MapsActivity.java
+++ app/src/main/java/com/example/livelocationapp/MapsActivity.java
@@ -40,7 +40,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         manager = (LocationManager) getSystemService(LOCATION_SERVICE);
         reference = FirebaseDatabase.getInstance().getReference().child("User-101");
-        //FirebaseDatabase.getInstance().getReference().setValue("Working Perfectly");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
